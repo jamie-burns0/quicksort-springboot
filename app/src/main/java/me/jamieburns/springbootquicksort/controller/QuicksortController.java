801273d09fe6d5bb14a637f91cf6539a52f1aeb4
jamie <jamie@jamieburns.me>
2024-06-18 10:08:55 +1000

    add CORS configuration

--- app/src/main/java/me/jamieburns/springbootquicksort/controller/QuicksortController.java
+++ app/src/main/java/me/jamieburns/springbootquicksort/controller/QuicksortController.java
@@ -3,9 +3,12 @@ package me.jamieburns.springbootquicksort.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import me.jamieburns.springbootquicksort.service.QuicksortService;
 
@@ -15,8 +18,20 @@ public class QuicksortController {
     @Autowired
     private QuicksortService quicksortService;
 
+    //@CrossOrigin(origins = "http://localhost:3000", maxAge = 300)
     @PostMapping("/quicksort")
     List<Integer> quicksort(@RequestBody List<Integer> list) {
         return quicksortService.quicksort(list);
+    }
+}
+
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/quicksort")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST")
+                .maxAge( 300 );
     }
 }
